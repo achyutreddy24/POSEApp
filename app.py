2f7c4e9101292ec12ec4001f9f16039311ab9a11
Achyut Reddy <achyutreddy24@gmail.com>
2019-02-03 00:34:56 -0500

    Update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 
-#from pyscripts import reddit
+from pyscripts import reddit
 from pyscripts import twitter
 
 app = Flask(__name__)
@@ -12,7 +12,7 @@ def result():
 @app.route('/', methods=['POST'])
 def form_post():
     text = request.form['text']
-    return twitter.find(text)
+    return twitter.find(text) + " " + reddit.find(text)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- pyscripts/reddit.py
+++ pyscripts/reddit.py
@@ -1,41 +1,13 @@
-import praw # simple interface to the reddit API, also handles rate limiting of requests
-import time
-import OAuth2Util
+from psaw import PushshiftAPI
 
 from pyscripts import sentiment
 
-#  Import Settings from Config.py
-try:
-    import Config
-    USERAGENT = Config.USERAGENT
-    MAXPOSTS = Config.MAXPOSTS
-    REPLYMESSAGE = Config.REPLYMESSAGE
-    
-    print("Loaded Config")
-except ImportError:
-    print("Error Importing Config.py")
-    
-WAIT = 5
-SENTIMENTSr = []
+api = PushshiftAPI()
 
-r = praw.Reddit(USERAGENT)
-#r.login(USERNAME, PASSWORD)
-o = OAuth2Util.OAuth2Util(r)
-
-def scan(keyword):
-    subreddit = r.get_subreddit("all")
-    stream = praw.helpers.comment_stream(r, subreddit, limit=100)
-    for comment in stream:
-        cbody = comment.body
-        if keyword not in cbody.lower():
-            continue
-        else:
-            SENTIMENTSr.append(sentiment.sentiment(comment))
-        
 def find(keyword):
-    SENTIMENTSr = []
-    for _ in range(1,50):
-        scan(keyword)
-        time.sleep(WAIT)
-    return sum(SENTIMENTSr)/len(SENTIMENTSr)
+    SENTIMENTS = []
+    gen = api.search_comments(q=keyword, limit=500)
+    for c in gen:
+        SENTIMENTS.append(sentiment.sentiment(c.body))
+    return repr(sum(SENTIMENTS)/len(SENTIMENTS))
 
