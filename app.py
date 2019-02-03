7040acee1970c88896852931bc3bba64ecc32ba8
Achyut Reddy <achyutreddy24@gmail.com>
2019-02-03 05:43:55 -0500

    Display sample texts

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def form_post():
     text = request.form['text']
     reddit_sent, reddit_extremes = reddit.find(text)
     twitter_sent, twitter_extremes = twitter.find(text)
-    return render_template("result.html", twitter = twitter_sent, reddit = reddit_sent)
+    return render_template("result.html", twitter = twitter_sent, reddit = reddit_sent, twitter_high = twitter_extremes["max"], twitter_low = twitter_extremes["min"], reddit_low = reddit_extremes["min"], reddit_high = reddit_extremes["max"])
 
 if __name__ == '__main__':
     app.run(debug=True)
