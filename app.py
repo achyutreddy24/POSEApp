b1f48d8646574fda0c5679b9df124430d0f58877
WilliamCao2018 <46179476+WilliamCao2018@users.noreply.github.com>
2019-02-03 01:19:46 -0500

    new changes
    dsajvndv

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def result():
 @app.route('/', methods=['POST'])
 def form_post():
     text = request.form['text']
-    return twitter.find(text) + " " + reddit.find(text)
+    return render_template("result.html", data = [twitter.find(text), reddit.find(text)]) 
 
 if __name__ == '__main__':
     app.run(debug=True)
